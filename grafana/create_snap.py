459f93f734e2f92bf5937f3278311378cf3a342c
Bohdan Leshchenko <bohdan.leshchenko1@gmail.com>
2022-05-16 21:10:36 +0200

    AConverted to a function, added logger

--- grafana/create_snap.py
+++ grafana/create_snap.py
@@ -1,55 +1,63 @@
 import requests
 import json
 import time
+import logging
 
-# Timestamp for snapshot
-timestr = time.strftime("%H:%M:%S_%Y/%m/%d")
+def create_snap():
 
-# Read API token from file
-with open('/home/cloud_user/api_token', 'r') as api_file:
-    api_token = api_file.read().strip('\n')
+    formatter = logging.Formatter(f"%(asctime)s %(levelname)s %(message)s ")
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    snap_logger = logging.getLogger()
+    snap_logger.addHandler(handler)
+    snap_logger.setLevel(logging.INFO)
+    
 
-# Auth for HTTP API
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_token}"
-}
+    # Timestamp for snapshot
+    timestr = time.strftime("%H:%M:%S_%Y/%m/%d")
 
-# Search for dashboard UID
-search_query = requests.get("http://localhost:3000/api/search?query=Telegraf%test%XD", headers=headers)
-load_query = json.loads(search_query.text)
-# Retrieving dashboard uid value
-for i in load_query:
-    dashboard_uid = i["uid"]
+    # Read API token from file
+    with open('/home/cloud_user/api_token', 'r') as api_file:
+        api_token = api_file.read().strip('\n')
+
+    # Auth for HTTP API
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_token}"
+    }
+    
+    search_url = "http://localhost:3000/api/search?query=Telegraf%test%XD"
+
+    # Search query for dashboard UID
+    search_query = requests.get(search_url, headers=headers)
+    load_query = json.loads(search_query.text)
+    # Retrieving dashboard uid value
+    # dashboard_uid = ast.literal_eval(load_query_parsed)
+    for keys in load_query:
+        dashboard_uid = keys["uid"]
 
 
-# Getting dashboard metadata by UID
-r_metadata = requests.get(f"http://localhost:3000/api/dashboards/uid/{dashboard_uid}", headers=headers)
-r_result = json.loads(r_metadata.text)
-snap_name = {"name": "Testing-snapshot-creation--" + timestr}
-r_result.update(snap_name)
-payload = json.dumps(r_result, indent=4, sort_keys=True)
+    metadata_url = f"http://localhost:3000/api/dashboards/uid/{dashboard_uid}"
+
+    # Getting dashboard metadata by UID
+    get_metadata = requests.get(metadata_url, headers=headers)
+    get_result = json.loads(get_metadata.text)
+    snap_name = {"name": "Testing-snapshot-creation--" + timestr}
+    get_result.update(snap_name)
+    payload = json.dumps(get_result, indent=4, sort_keys=True)
+
+    # Creating snapshot
+    url = "http://localhost:3000/api/snapshots"
+
+    post_data = requests.post(url, headers=headers, data=payload)
+    post_parsed = json.loads(post_data.text)
+    # print(p)
+    # print(p.status_code)
+    post_snap_url = post_parsed["url"]
+
+    logging.info(f"Snapshot has been created for dashboard UID: {dashboard_uid} Snapshot URL: {post_snap_url}")
 
 
-# Writing data to a json file
-# with open('test_data.json', 'w') as f:
-#     data = json.dumps(data, indent=4, sort_keys=True)
-#     f.write(data)
-
-# Update json file with snap-name
-# snapshot_name = {"name": "Testing-snapshot-creation"}
-
-# Creating a snapshot from file
-
-# with open('test_data.json', 'r') as json_file:
-#     payload = json.load(json_file)
-
-# Creating snapshot
-url = "http://localhost:3000/api/snapshots"
-
-p = requests.post(url, headers=headers, data=payload)
-parsed = json.loads(p.text)
-# print(p)
-# print(p.status_code)
-print(json.dumps(parsed, indent=4, sort_keys=True))
+create_snap()
