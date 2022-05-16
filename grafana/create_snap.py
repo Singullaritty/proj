e2a1254602d2be40dd3166b0963d7d5db1564e24
Bohdan Leshchenko <bohdan.leshchenko1@gmail.com>
2022-05-16 21:29:41 +0200

    Cosmetic changes

--- grafana/create_snap.py
+++ grafana/create_snap.py
@@ -13,7 +13,6 @@ def create_snap():
     snap_logger.addHandler(handler)
     snap_logger.setLevel(logging.INFO)
     
-
     # Timestamp for snapshot
     timestr = time.strftime("%H:%M:%S_%Y/%m/%d")
 
@@ -28,20 +27,16 @@ def create_snap():
         "Authorization": f"Bearer {api_token}"
     }
     
-    search_url = "http://localhost:3000/api/search?query=Telegraf%test%XD"
-
     # Search query for dashboard UID
+    search_url = "http://localhost:3000/api/search?query=Telegraf%test%XD"
     search_query = requests.get(search_url, headers=headers)
     load_query = json.loads(search_query.text)
     # Retrieving dashboard uid value
-    # dashboard_uid = ast.literal_eval(load_query_parsed)
     for keys in load_query:
         dashboard_uid = keys["uid"]
 
-
-    metadata_url = f"http://localhost:3000/api/dashboards/uid/{dashboard_uid}"
-
     # Getting dashboard metadata by UID
+    metadata_url = f"http://localhost:3000/api/dashboards/uid/{dashboard_uid}"
     get_metadata = requests.get(metadata_url, headers=headers)
     get_result = json.loads(get_metadata.text)
     snap_name = {"name": "Testing-snapshot-creation--" + timestr}
@@ -58,6 +53,5 @@ def create_snap():
     post_snap_url = post_parsed["url"]
 
     logging.info(f"Snapshot has been created for dashboard UID: {dashboard_uid} Snapshot URL: {post_snap_url}")
-
 
 create_snap()
