239ec7431dcc821d830ea6072f343b665406c944
Bohdan Leshchenko <bohdan.leshchenko1@gmail.com>
2022-05-25 13:24:11 +0200

    created new profram for working with json data

--- grafana/collect_data.py
+++ grafana/collect_data.py
@@ -1,8 +1,8 @@
-from wsgiref.headers import Headers
 import requests
 import json
 import datetime
 import sys
+import ijson
 from datetime import timedelta
 from loguru import logger
 
@@ -44,29 +44,18 @@ def create_snap():
     metadata_url = f"http://localhost:3000/api/dashboards/uid/{dashboard_uid}"
     get_metadata = requests.get(metadata_url, headers=headers)
     get_result = json.loads(get_metadata.text)
-    time_value = {
-            "from": f"{one_hour_minus}",
-            "to": f"{today}",
-            "raw": {
-                "from": "now-1h",
-                "to": "now"
-                }
-    }
-    snap_name = {"name": "Testing-snapshot-creation--" + snap_time}
-    get_result['dashboard']['time'] = time_value
-    get_result.update(snap_name)
     payload = json.dumps(get_result, indent=3, sort_keys=True)
 
-    # with open('pay_test.json', 'r') as json_file:
-    #     file_read = json_file.read()
-    #     data_loads = json.loads(file_read)
-    #     print(json.dumps(data_loads, indent=3, sort_keys=True))
+    # Write to a file
+    with open('sample.json', 'w') as outfile:
+        outfile.write(payload)
 
-    # # Creating snapshot
-    # snapshot_url = "http://localhost:3000/api/snapshots"
-    # post_data = requests.post(snapshot_url, headers=headers, data=payload)
-    # post_parsed = json.loads(post_data.text)
-    # post_snap_url = post_parsed["url"]
-    # logger.info(f"Snapshot for dashboard: \"{dashboard_name}\" has been created. It can be accessed via URL: {post_snap_url}", format=frmt, colorize=True)
+def parse_json(json_filename):
+    with open(json_filename, 'rb') as input_file:
+        # load json
+        parser = ijson.parse(input_file)
+        for prefix, event, value in parser:
+            print(f'prefix={prefix}, event={event}, value={value}')
 
 create_snap()
+parse_json('sample.json')
