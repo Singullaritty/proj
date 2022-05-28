0469e8c2d10373d239d7cfbfd9b2650025db4b2b
Bohdan Leshchenko <bohdan.leshchenko1@gmail.com>
2022-05-28 13:57:41 +0200

    added new fucntions. Created main function for extract data with nested functions

--- grafana/collect_data.py
+++ grafana/collect_data.py
@@ -13,6 +13,7 @@ database_name = 'test'
 logger.remove(0)
 frmt =  "<green>{time}</green> | <blue>{level}</blue> | {message}"
 logger.add(sys.stderr, level="INFO", format=frmt)
+logger.add(sys.stderr, level="DEBUG")
 
 # Read API token from file
 with open('/home/cloud_user/api_token', 'r') as api_file:
@@ -32,6 +33,7 @@ def load_json(object):
     return json.loads(object.text)
 
 def search_dashboard(header):
+    logger.debug("load_json")
     # Search query for dashboard UID
     search_url = f"http://localhost:3000/api/search?query=Telegraf%test%XD"
     get_dashboard = load_json(requests.get(search_url, headers=header))
@@ -40,6 +42,7 @@ def search_dashboard(header):
     return dashboard_uid
 
 def get_dashboard_metadata(dashboard_uid, header):
+    logger.debug("get_dashboard_metadata")
     # Getting dashboard metadata by UID
     metadata_url = f"http://localhost:3000/api/dashboards/uid/{dashboard_uid}"
     get_metadata = load_json(requests.get(metadata_url, headers=header))
@@ -49,6 +52,7 @@ def get_dashboard_metadata(dashboard_uid, header):
     return dict(get_metadata), json_f.name
 
 def parse_json(json_filename):
+    logger.debug("parse_json")
     with open(json_filename, 'rb') as input_file:
         # load json iteratively
         data = []
@@ -58,32 +62,53 @@ def parse_json(json_filename):
         nl_data = '\n'.join(data)
         return str(nl_data).strip('[|]').replace('\'','')
 
-def extract_ds_queries(json_filename):
-    with open(json_filename, 'rb') as input_file:
-        q_list = []
-        queries = ijson.items(input_file, 'dashboard.panels.item.targets.item.query')
-        for query in queries:
-            q_list.append(f'{query}')
-        q_list = [w.replace('$timeFilter', 'time >= now() - 1h and time <= now()') for w in q_list]
-        q_list = [w.replace('$interval', '30s') for w in q_list]
-        q_list = [w.replace('$server', 'learn-python') for w in q_list]
-        q_list = [w.replace(' ', '%20') for w in q_list]
-        q_list = [w.replace('"', '%22') for w in q_list]
-        return q_list
+def extract_data(json_filename, header):
+    logger.debug("extract_ds_queries")
+    # extract list of the queries from dashboard metadata
+    def extract_queries_list():
+        logger.debug("extract_queries_list")
+        with open(json_filename, 'rb') as input_file:
+            q_list = []
+            queries = ijson.items(input_file, 'dashboard.panels.item.targets.item.query')
+            for query in queries:
+                q_list.append(f'{query}')
+            q_list = [w.replace('$timeFilter', 'time >= now() - 1h and time <= now()') for w in q_list]
+            q_list = [w.replace('$interval', '30s') for w in q_list]
+            q_list = [w.replace('$server', 'learn-python') for w in q_list]
+            q_list = [w.replace(',', '%2C') for w in q_list]
+            q_list = [w.replace(' ', '%20') for w in q_list]
+            q_list = [w.replace('"', '%22') for w in q_list]
+            q_list = [s + '&epoch=ms' for s in q_list]
+            return q_list
+    # extract data for snapshot template
+    def extract_snap_templ_data():
+        logger.debug("extract_snap_templ_data")
+        pass
 
-def get_ds_values(header, queries_list):
-    for query in queries_list:
-        query_url = f"http://localhost:3000/api/datasources/proxy/1/query?db={database_name}&q={query}"
-        query_result = load_json(requests.get(query_url, headers=header))
-        print(query_result)
-    #return query_result
+    def get_ds_values():
+        logger.debug("get_ds_values")
+        response_q = []
+        for query in extract_queries_list():
+            query_url = f"http://localhost:3000/api/datasources/proxy/1/query?db={database_name}&q={query}"
+            query_result = load_json(requests.get(query_url, headers=header))
+            for data in query_result.items():
+                response_q.append(f'{data}')
+        return response_q
+    
+    # extract values from executed queries
+    def extract_ds_values():
+        logger.debug("extract_ds_values")
+        pass
+
+    return extract_queries_list(), get_ds_values()
+
+
 
 def main():
     uid = search_dashboard(logon)
     metadata = get_dashboard_metadata(uid, logon)[0]
     file = get_dashboard_metadata(uid, logon)[1]
-    queries_list = extract_ds_queries(file)
-    ds_values = get_ds_values(logon, queries_list)
+    queries_list, ds_values = extract_data(file, logon)[0], extract_data(file, logon)[1]
     print(ds_values)
 
 if __name__ == "__main__":
