77e1d095b5dce75528482115850dac182081aa66
Bohdan Leshchenko <bohdan.leshchenko1@gmail.com>
2022-05-29 18:08:30 +0200

    add function to retrieve snap template data

--- grafana/collect_data.py
+++ grafana/collect_data.py
@@ -3,7 +3,7 @@ import json
 import datetime
 import sys
 import ijson
-import io
+import asyncio
 from datetime import timedelta
 from loguru import logger
 
@@ -71,54 +71,82 @@ def parse_json(json_filename):
         return str(nl_data).strip('[|]').replace('\'','')
 
 # extracting data for snapshot template
-def extract_data(json_filename, header):
-    logger.debug("extract_ds_queries")
+def extract_json_data(json_filename, header):
+    logger.debug("extract_json_data")
     # extract list of the queries from dashboard metadata
-    def extract_queries_list():
-        logger.debug("extract_queries_list")
+    def extract_all_data():
+        snap_data_list = []
+
+        logger.debug("extract_all_data")
         with open(json_filename, 'rb') as input_file:
-            q_list = []
-            queries = ijson.items(input_file, 'dashboard.panels.item.targets.item.query')
-            for query in queries:
-                q_list.append(f'{query}')
-            q_list = [w.replace('$timeFilter', 'time >= now() - 1h and time <= now()') for w in q_list]
-            q_list = [w.replace('$interval', '30s') for w in q_list]
-            q_list = [w.replace('$server', 'learn-python') for w in q_list]
-            q_list = [w.replace(',', '%2C') for w in q_list]
-            q_list = [w.replace(' ', '%20') for w in q_list]
-            q_list = [w.replace('"', '%22') for w in q_list]
-            q_list = [s + '&epoch=ms' for s in q_list]
-            return q_list
+            color_mode = ijson.items(input_file, 'dashboard.panels.item.fieldConfig.defaults.color.mode')
+            thresholds_mode = ijson.items(input_file, 'dashboard.panels.item.fieldConfig.defaults.thresholds.mode')
+            thresholds_data = ijson.items(input_file, 'dashboard.panels.item.fieldConfig.defaults.thresholds', multiple_values=True)
+            
+            for itemc in color_mode:
+                print(f"Color modes: \n{itemc}\n")
+            input_file.seek(0, 0)
+
+            for thm in thresholds_mode:
+                print(f"Thresholds modes: \n{thm} \n")
+            input_file.seek(0, 0)
+
+            for thd in thresholds_data:
+                print(f"Thresholds data: \n{thd} \n")
+            input_file.seek(0, 0)
+
+            # for query in queries:
+            #     snap_data_list.append(f'{query}')
+            #     snap_data_list = [w.replace('$timeFilter', 'time >= now() - 1h and time <= now()') for w in snap_data_list]
+            #     snap_data_list = [w.replace('$interval', '30s') for w in snap_data_list]
+            #     snap_data_list = [w.replace('$server', 'learn-python') for w in snap_data_list]
+            #     snap_data_list = [w.replace(',', '%2C') for w in snap_data_list]
+            #     snap_data_list = [w.replace(' ', '%20') for w in snap_data_list]
+            #     snap_data_list = [w.replace('"', '%22') for w in snap_data_list]
+            #     snap_data_list = [s + '&epoch=ms' for s in snap_data_list]
+
+            # for c_mode in color_mode:
+            #     snap_data_list.append(f"{c_mode}")
+
+            # for thrm in thresholds_mode:
+            #     snap_data_list.append(f"{thrm}")
+
+            # for thrd in thresholds_data:
+            #     snap_data_list.append(f"{thrd}")
+
+        return snap_data_list
+
     # extract data for snapshot template
     def extract_snap_templ_data():
         logger.debug("extract_snap_templ_data")
         pass
     
     # execute queries and return results as list
-    def get_ds_values():
-        logger.debug("get_ds_values")
-        response_q = []
-        for query in extract_queries_list():
-            query_url = f"http://localhost:3000/api/datasources/proxy/1/query?db={database_name}&q={query}"
-            query_result = load_json(requests.get(query_url, headers=header))
-            for data in query_result.items():
-                response_q.append(f'{data}')
-        return response_q
+    # def get_ds_values():
+    #     logger.debug("get_ds_values")
+    #     response_q = []
+    #     for query in extract_all_data():
+    #         query_url = f"http://localhost:3000/api/datasources/proxy/1/query?db={database_name}&q={query}"
+    #         query_result = load_json(requests.get(query_url, headers=header))
+    #         for data in query_result.items():
+    #             response_q.append(f'{data}')
+    #     return response_q
     
     # extract values from executed queries
     def extract_ds_values():
         logger.debug("extract_ds_values")
         pass
 
-    return extract_queries_list(), get_ds_values()
+    return extract_all_data()#, get_ds_values()
 
 # main function
 def main():
     #print(get_dashboard(logon)[0][0])
     uid = get_dashboard(logon)[1]
     metadata, file_to_parse = get_dashboard(logon)[0][0], get_dashboard(logon)[0][1]
-    queries_list, ds_values = extract_data(file_to_parse, logon)[0], extract_data(file_to_parse, logon)[1]
-    print(ds_values[1])
+    #queries_list, ds_values = 
+    extract_json_data(file_to_parse, logon)#[0], extract_json_data(file_to_parse, logon)[1]
+    #print(ds_values[1])
 
 if __name__ == "__main__":
     main()
